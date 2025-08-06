e2de779164100a0633282289bd55bbfc52bfdc3a
Christian Zufferey <christian@zufferey.com>
2025-08-06 14:55:50 +0200

    new: cela commence à fonctionner, mais ce n'est pas encore pratique pour le moment

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 # petit script pour démarrer un serveur Elasticsearch
-# zf250806.1216
+# zf250806.1339
 
-# sources: https://hub.docker.com/_/elasticsearch
+# sources:
+# https://hub.docker.com/_/elasticsearch
+# https://www.elastic.co/guide/en/elasticsearch/reference/7.5/docker.html
 
-docker run --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:tag
+#docker network create somenetwork
+#docker run --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:tag
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.5.2
+#docker.elastic.co/elasticsearch/elasticsearch:9.1.0
 
 
 exit
