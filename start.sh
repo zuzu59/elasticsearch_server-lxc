d896aa2e03f9899427eca5eab8be8ece8bf63146
Christian Zufferey <christian@zufferey.com>
2025-08-06 17:01:52 +0200

    new: nouveaux srcipts pour démarrer et arrêter facilement le serveur Elasticsearch

--- start.sh
+++ start.sh
@@ -1,21 +1,5 @@
-# petit script pour démarrer un serveur Elasticsearch
-# zf250806.1339
+# petit script pour démarrer Elasticsearch Server
+# zf250806.1700
 
-# sources:
-# https://hub.docker.com/_/elasticsearch
-# https://www.elastic.co/guide/en/elasticsearch/reference/7.5/docker.html
+docker compose up -d
 
-#docker network create somenetwork
-#docker run --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:tag
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.5.2
-#docker.elastic.co/elasticsearch/elasticsearch:9.1.0
-
-
-exit
-
-
-docker run -t -d -p 9980:9980 \
-    -e 'domain=cloud\\.nextcloud\\.com' \
-    --restart always \
-    --cap-add MKNOD \
-    collabora/code
---
+++ stop.sh
@@ -0,0 +1,4 @@
+# petit script pour arrêter Elasticsearch Server
+# zf250806.1700
+
+docker compose stop
